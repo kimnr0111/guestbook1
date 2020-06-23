1a3db59cdd3f1bb88b79a18abbd161b4e21e969c
hi <hi@DESKTOP-G463TQN>
2020-06-23 10:23:52 +0900

    3

--- src/com/javaex/dao/GuestDao.java
+++ src/com/javaex/dao/GuestDao.java
@@ -95,7 +95,8 @@ public class GuestDao {
 			query += "         password, ";
 			query += "         content, ";
 			query += "         reg_date ";
-			query += " from guestbook";
+			query += " from guestbook ";
+			query += " order by no asc ";
 			
 			pstmt = conn.prepareStatement(query); // 쿼리로 만들기
 			
